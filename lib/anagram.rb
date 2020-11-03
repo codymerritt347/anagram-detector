b39c76443e7de44554e3ee34afb630e2fdced0f2
Cody Merritt <cody.merritt@me.com>
2020-11-03 13:10:36 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+require "pry"
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(name)
+    @word_sorted = name.split("").sort
+  end
+
+  # binding.pry
+
+  def match(array_of_words)
+    matches = []
+    array_of_words.collect do |word|
+      matched_word = word.split("").sort!
+      if matched_word == @word_sorted
+        matches << word
+      else
+      end
+    end
+    matches
+  end
+
+end
